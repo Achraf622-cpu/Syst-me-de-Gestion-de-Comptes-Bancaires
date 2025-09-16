8322fa25b868ea753d49ff5415fa1f76496099ab
Achraf Hanzaz <hanzazachraf581@gmail.com>
2025-09-17 00:51:21 +0100

    BANK-3 : Creation de la class compte

---
+++ src/bank/model/Compte.java
@@ -0,0 +1,18 @@
+package bank.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+
+public abstract class Compte {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
